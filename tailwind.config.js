86fdd5519eaebf14d3270df8bb522475279d108d
Rafi Ferdos <rafiferdos@gmail.com>
2024-05-31 22:40:17 +0600

    chore: Update project configuration and dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ['Nunito Sans', 'sans-serif'],
+        work: ['Work Sans', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
+
