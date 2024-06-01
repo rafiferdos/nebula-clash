d30dbe5ee55ee634e68fa41a70b7070555d539a7
Rafi Ferdos <rafiferdos@gmail.com>
2024-06-01 12:50:45 +0600

    refactor: Update Home page with Banner component for improved visual appeal

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,9 @@
+import Banner from "../../components/Banner";
+
 const Home = () => {
     return (
-        <div>
-            <p className="text-4xl">homel Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur libero, sapiente, possimus iste nesciunt tempore alias sint voluptate dolorum deleniti esse, est sit necessitatibus consectetur eius deserunt? Odit alias explicabo dolore minus commodi accusantium voluptas rem ea. Voluptatem, laborum porro?</p>
+        <div className="container mx-auto my-8 max-w-7xl w-11/12 md:space-y-10 space-y-8 lg:space-y-16">
+            <Banner />
         </div>
     );
 };
