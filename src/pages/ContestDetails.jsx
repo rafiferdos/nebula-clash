78a362f01b21853df3a3d60af88e3f3e810b1c58
Rafi Ferdos <rafiferdos@gmail.com>
2024-06-05 02:26:09 +0600

    refactor: Update ContestDetails page with countdown timer and loading skeleton

--- src/pages/ContestDetails.jsx
+++ src/pages/ContestDetails.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { IoMdArrowBack } from "react-icons/io";
 import useAxiosCommon from "../hooks/useAxiosCommon";
+import { useEffect, useState } from "react";
 
 const ContestDetails = () => {
 
@@ -17,6 +18,41 @@ const ContestDetails = () => {
         }
     })
 
+    const calculateTimeLeft = (deadline) => {
+        if (!deadline) {
+          return {
+            days: 0,
+            hours: 0,
+            minutes: 0,
+            seconds: 0,
+          };
+        }
+      
+        const difference = +new Date(deadline) - +new Date();
+        let timeLeft = {};
+      
+        if (difference > 0) {
+          timeLeft = {
+            days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+            hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+            minutes: Math.floor((difference / 1000 / 60) % 60),
+            seconds: Math.floor((difference / 1000) % 60),
+          };
+        }
+      
+        return timeLeft;
+      };
+
+      const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(data?.deadline));
+
+      useEffect(() => {
+        const timer = setInterval(() => {
+            setTimeLeft(calculateTimeLeft(data?.deadline));
+          }, 1000);
+    
+        return () => clearTimeout(timer);
+      }, [data?.deadline]);
+
     // const {name, description, image, participants} = data
 
     // console.log(data)
@@ -28,9 +64,13 @@ const ContestDetails = () => {
                 isLoading ?
                     <div className="card lg:card-side bg-base-100 shadow-xl lg:p-10 rounded-3xl p-4 md:p-7">
                         <div className="skeleton lg:w-96 h-72" alt="Album" />
-                        <div className="card-body space-y-5">
+                        <div className="card-body space-y-3 p-4 md:p-7 mt-4">
                             <div className="h-12 w-full skeleton"></div>
-                            <p className="h-20 w-full skeleton"></p>
+                            <div className="h-6 w-full skeleton"></div>
+                            <div className="h-20 w-full skeleton"></div>
+                            <div className="card-actions justify-start">
+                                <div className="h-12 w-20 skeleton"></div>
+                            </div>
                             <div className="card-actions justify-end">
                                 <div className="h-12 w-20 skeleton"></div>
                             </div>
@@ -38,12 +78,44 @@ const ContestDetails = () => {
                     </div>
                     :
                     <div className="card lg:card-side bg-base-100 shadow-xl lg:p-10 rounded-3xl p-4 md:p-7 glass">
-                        <figure className="shadow-2xl"><img src={data.image} className="rounded-3xl" alt="Album" /></figure>
+                        <figure className="lg:max-w-xl"><img src={data.image} className="rounded-3xl" alt="Album" /></figure>
                         <div className="card-body p-4 md:p-7 space-y-5 mt-4">
-                            <h2 className="card-title">{data.name}</h2>
+                            <h2 className="card-title font-nunito text-2xl md:text-4xl lg:text-5xl">{data.name}</h2>
+                            <p className="font-nunito font-bold">Participants: {data.participants.length}</p>
                             <p>{data.description}</p>
+                            <p>Contest Prize: {data.prize} taka</p>
+                                <h2 className="text-xl md:text-3xl lg:text-3xl">Time Remaining:</h2>
+                            <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
+                                <div className="flex flex-col">
+                                    <span className="countdown font-mono text-xl md:text-5xl">
+                                        <span style={{ "--value": timeLeft.days }}></span>
+                                    </span>
+                                    days
+                                </div>
+                                <div className="flex flex-col">
+                                    <span className="countdown font-mono text-xl md:text-5xl">
+                                        <span style={{ "--value": timeLeft.hours }}></span>
+                                    </span>
+                                    hours
+                                </div>
+                                <div className="flex flex-col">
+                                    <span className="countdown font-mono text-xl md:text-5xl">
+                                        <span style={{ "--value": timeLeft.minutes }}></span>
+                                    </span>
+                                    min
+                                </div>
+                                <div className="flex flex-col">
+                                    <span className="countdown font-mono text-xl md:text-5xl">
+                                        <span style={{ "--value": timeLeft.seconds }}></span>
+                                    </span>
+                                    sec
+                                </div>
+                            </div>
+                            <div className="card-actions justify-start">
+                                <button className="btn glass rounded-full bg-base-300" onClick={() => window.history.back()}>Register</button>
+                            </div>
                             <div className="card-actions justify-end">
-                                <button className="btn glass rounded-full bg-base-300" onClick={() => window.history.back()}><IoMdArrowBack /> Go Back</button>
+                                <button className="btn glass rounded-full bg-base-300" onClick={() => window.history.back()}><IoMdArrowBack /></button>
                             </div>
                         </div>
                     </div>
